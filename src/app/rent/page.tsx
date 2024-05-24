9f9ce5efcab2ed4710696bf66bd19e9360e84384
Engraya <ahmadyakubuaya@gmail.com>
2024-05-24 06:47:06 +0200

    Fix

--- src/app/rent/page.tsx
+++ src/app/rent/page.tsx
@@ -1,42 +1,42 @@
-import SingleProperty from "@/components/Buy/SingleProperty";
-import Breadcrumb from "@/components/Common/Breadcrumb";
-import { getForRentData } from "utils/dataFetch";
-import { Metadata } from "next";
+// import SingleProperty from "@/components/Buy/SingleProperty";
+// import Breadcrumb from "@/components/Common/Breadcrumb";
+// import { getForRentData } from "utils/dataFetch";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Rent Page | Shelterium",
-  description: "This is a Page for Rent",
-  // other metadata
-};
+// export const metadata: Metadata = {
+//   title: "Rent Page | Shelterium",
+//   description: "This is a Page for Rent",
+//   // other metadata
+// };
 
-const RentPage = async () => {
+// const RentPage = async () => {
 
-  const propertyForRentData = await getForRentData();
+//   const propertyForRentData = await getForRentData();
 
-  const path = "rent"
+//   const path = "rent"
 
-  return (
-    <>
-      <Breadcrumb
-        pageName="Buy Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
-      />
-      <section className="pb-[120px] pt-[80px]">
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap justify-center">
-            {propertyForRentData?.map((property) => (
-              <div
-                key={property?.id}
-                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-              >
-                <SingleProperty property={property} path={path} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Breadcrumb
+//         pageName="Buy Page"
+//         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+//       />
+//       <section className="pb-[120px] pt-[80px]">
+//         <div className="container">
+//           <div className="-mx-4 flex flex-wrap justify-center">
+//             {propertyForRentData?.map((property) => (
+//               <div
+//                 key={property?.id}
+//                 className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
+//               >
+//                 <SingleProperty property={property} path={path} />
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
 
-export default RentPage;
+// export default RentPage;
